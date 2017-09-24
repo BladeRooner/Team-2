203971f406a27140d33c2bda5b6497b871773ba8
cmckillop <conor303@gmail.com>
2017-09-24 19:00:27 +0100

    Restore Particle.js Functionality
    Fix script source for particle.js on Login page. (Sorry)

--- login.php
+++ login.php
@@ -63,7 +63,7 @@
             </div>
         </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js" /script>
+        <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
 
         <script>
             particlesJS.load('particles-js', 'assets/particles-js.json');
